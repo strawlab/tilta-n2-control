25aaa7745a83614799a7df8bf6cc809d67069416
DeepSOIC <vv.titov@gmail.com>
2024-02-12 17:54:45 +0100

    tilta is moving!
    but slowly

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
+#include <nrf_802154.h>
+
+typedef uint8_t byte;
+
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
@@ -18,12 +22,27 @@
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led_r = GPIO_DT_SPEC_GET(DT_ALIAS(led1_red), gpios);
+static const struct gpio_dt_spec led_b = GPIO_DT_SPEC_GET(DT_ALIAS(led1_blue), gpios);
+
+void blink(){
+	gpio_pin_set_dt(&led, true);
+	k_msleep(1000);
+	gpio_pin_set_dt(&led, false);
+	k_msleep(100);
+}
+
+void set_pos(byte* data_to_transmit, int val){
+	data_to_transmit[10] = 0x40 | (byte)((val >> 8) & 0x0F);
+	data_to_transmit[11] = (byte)(val & 0xFF);
+	data_to_transmit[13] = 0 - data_to_transmit[10] - data_to_transmit[11] - data_to_transmit[12];
+}
 
 int main(void)
 {
 	int ret;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!(gpio_is_ready_dt(&led) && gpio_is_ready_dt(&led_r) && gpio_is_ready_dt(&led_b))) {
 		return 0;
 	}
 
@@ -32,12 +51,33 @@ int main(void)
 		return 0;
 	}
 
+	gpio_pin_configure_dt(&led_r, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_configure_dt(&led_b, GPIO_OUTPUT_INACTIVE);
+	gpio_pin_set_dt(&led_b, false);
+
+	byte data_to_transmit[] = 
+		"\x0f\x61\x88\xC4\xE7\x01\xFF\xFF\x96\xF0\x44\x05\x00\xB7\x00\x00";
+	//     ^^ packet length (with FCS)
+	//                                                             ^^  ^^  space for FCS (packet crc), computed by tx function	
+
+	blink();
+	nrf_802154_init();
+	blink();
+	nrf_802154_channel_set(12);
+	blink();
+
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-		k_msleep(SLEEP_TIME_MS);
+		set_pos(data_to_transmit, 0);
+		ret = nrf_802154_transmit_raw(data_to_transmit, NULL);
+		gpio_pin_set_dt(&led_b, ret);
+		
+		k_msleep(2000);
+
+		set_pos(data_to_transmit, 1000);
+		ret = nrf_802154_transmit_raw(data_to_transmit, NULL);
+		gpio_pin_set_dt(&led_b, ret);
+
+		k_msleep(2000);
 	}
 	return 0;
 }
