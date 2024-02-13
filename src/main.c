a652476999db6442df47f325c3a12bd60511ca69
DeepSOIC <vv.titov@gmail.com>
2024-02-13 14:54:41 +0100

    clean up and comment, adjust the license note

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2016 Intel Corporation
- *
+ * Copyright (c) 2024 Straw Lab
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -11,26 +10,9 @@
 
 typedef uint8_t byte;
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct gpio_dt_spec led_r = GPIO_DT_SPEC_GET(DT_ALIAS(led1_red), gpios);
 static const struct gpio_dt_spec led_b = GPIO_DT_SPEC_GET(DT_ALIAS(led1_blue), gpios);
-
-void blink(){
-	gpio_pin_set_dt(&led, true);
-	k_msleep(200);
-	gpio_pin_set_dt(&led, false);
-	k_msleep(50);
-}
 
 void set_pos(byte* data_to_transmit, int val){
 	data_to_transmit[10] = 0x40 | (byte)((val >> 8) & 0x0F);
@@ -46,38 +28,30 @@ int main(void)
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
-
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_INACTIVE);
 	gpio_pin_configure_dt(&led_r, GPIO_OUTPUT_INACTIVE);
-	gpio_pin_configure_dt(&led_b, GPIO_OUTPUT_INACTIVE);
-	gpio_pin_set_dt(&led_b, false);
 
 	byte data_to_transmit[] = 
 		"\x0f\x61\x88\x00\xE7\x01\xFF\xFF\x96\xF0\x44\x05\x00\xB7\x00\x00";
 	//     ^^ packet length (with FCS)
-	//                                                             ^^  ^^  space for FCS (packet crc), computed by tx function	
+	//                                                             ^^  ^^  space for FCS (packet crc, computed by nrf_802154_transmit_raw)	
 
-	blink();
 	nrf_802154_init();
-	blink();
 	nrf_802154_channel_set(12);
-	blink();
 
 	int n = 0;
 
 	while (1) {
 		bool toggle = ((n / 200) % 2 == 0);
-		set_pos(data_to_transmit,  toggle ? n % 2 : 1000 + (n % 2));
-		gpio_pin_set_dt(&led_r, toggle);
-		ret = nrf_802154_transmit_raw(data_to_transmit, NULL);
-		gpio_pin_set_dt(&led_b, ret);
+		set_pos(data_to_transmit,  toggle ? n % 2 : 1000 + (n % 2)); //(n%2) is adding some noise to the data, that keeps the motor at full speed
+		gpio_pin_set_dt(&led_r, toggle); //toggle red together with position
+		nrf_802154_transmit_raw(data_to_transmit, NULL);
 		
-		k_msleep(10);
+		k_msleep(10); //ideally, we should also ensure that tx has finished before editing the buffer... skipping that for simplicity
 		++n;
-		++data_to_transmit[3];
+		++data_to_transmit[3]; //increment packet sequence number
+
+		gpio_pin_set_dt(&led,true); //indicate that everything is running
 	}
 	return 0;
 }
